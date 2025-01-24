73aab11eb7c02f7904b2f72eed2bf64c22ee1f43
Ryan Montagno <ryan.montagno@outlook.com>
2025-01-23 23:31:12 -0500

    Initial commit.

---
+++ src/test/java/com/rmont/e_comm_app/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmont.e_comm_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
